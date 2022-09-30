103e109a8658035ef0df52f315b19bf3fbe9f7b7
Dalyn Dalton <dalyn.dalton.dd@gmail.com>
2022-09-30 09:01:42 -0600

    Moved the frontend demo into its own dedicated folder along with build instructions, added class diagram to main readme

---
+++ demos/frontend/src/pages/SettingsPage.tsx
@@ -0,0 +1,12 @@
+import NavigationBar from "../components/NavigationBar/NavigationBar";
+
+
+export default function SettingsPage() {
+  return (
+    <div>
+      <NavigationBar />
+      <p>This is the Settings page</p>
+      <p>Here you can change the settings</p>
+    </div>
+  );
+}
