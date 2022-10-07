336675e438c33a1ba281a3c57809bfc44949cdb9
Dalyn Dalton <dalyn.dalton.dd@gmail.com>
2022-10-06 18:05:49 -0600

    set up the backend framework folder (#70)

---
+++ src/server/frappy/frappapi/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Frappe
+class FrappeSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Frappe
+        fields = []
