89ac7b591688ab4b4fa3c94c9daac7512d967a94
Dalyn Dalton <dalyn.dalton.dd@gmail.com>
2022-10-23 16:21:38 -0600

    forgot to add the ps script to the commit that fixes the bug

--- src/server/init.ps1
+++ src/server/init.ps1
@@ -2,7 +2,10 @@
 
 docker build -t frappe_server ./
 docker run -d --name frappe -p 5432:5432/tcp frappe_server
+docker container start frappe
 
+Write-Output "Waiting on server start..."
+Start-Sleep -Seconds 1.5
 # Step 2: have python load in the data
 Set-Location frappy
 ..\.venv\Scripts\python.exe manage.py migrate --fake-initial
