55e819fce6631b1f488f0b24be2bea7a2562cd8f
jfitzusu <A02261889@usu.edu>
2022-10-24 15:13:02 -0600

    Added Basic Hour Tracker

--- src/frontend/src/pages/HoursModal.tsx
+++ src/frontend/src/pages/HoursModal.tsx
@@ -7,10 +7,7 @@ interface Props {
 }
 
 export default function HoursModal(props: Props) {
-  const [start, setStart] = useState('');
-  const [end, setEnd] = useState('');
-
-  const [newTotal, setNewTotal] = useState(String(props.currentHours));
+  const [hours, setHours] = useState('');
 
   function handleConfirm() {
     props.setModalIsOpen(false);
@@ -22,18 +19,20 @@ export default function HoursModal(props: Props) {
 
   return (
     <div className="hours-container">
-      <div className="hours-home-title">Enter Hours</div>
+      <div className="hours-home-title">Enter Hours Worked</div>
       <div className="hours-step-container">
         <input
           className="hours-input"
           type="number"
           step="1"
-          onChange={(e) => setStart(e.target.value)}
+          onChange={(e) => setHours(e.target.value)}
         ></input>
       </div>
       <div className="new-hours">
         <u className="hours-text-boi">Updated Hours:</u>
-        <div className="hours-value">{newTotal} Hrs</div>
+        <div className="hours-value">
+          {props.currentHours + Number(hours)} Hrs
+        </div>
       </div>
       <div className="hours-buttons">
         <div className="hours-button cancel" onClick={handleCancel}>
