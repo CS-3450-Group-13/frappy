a03d752e2ce72a989ba8cb481f60e0c0da433f7a
Spencer Peterson <spencerthomasp@gmail.com>
2022-12-04 15:31:52 -0700

    Remove unused test

--- src/frontend/src/tests/Queue.test.tsx
+++ src/frontend/src/tests/Queue.test.tsx
@@ -16,49 +16,4 @@ describe('Customer Cart Page Tests', () => {
 
     expect(screen.getByText(/BARISTA QUEUE/i)).toBeInTheDocument();
   });
-
-  // test('Page renders with pending orders', () => {
-  //   let testFrappe: CashierFrappe = {
-  //     id: 1,
-  //     user: 0,
-  //     milk: MilkOptions.Soy_Milk,
-  //     base: BaseOptions.Coffee,
-  //     extras: [
-  //       {amount: 1, extras: ExtraOptions.Caramel, frappe: 1}
-  //     ],
-  //     price: 8.84,
-  //     final_price: 8.84,
-  //     menu_key: 1,
-  //     creator: 'John Doe',
-  //     size: SizeOptions.Small,
-  //     comments: '',
-  //     create_date: '',
-  //     status: StatusOptions.In_Progress,
-  //   }
-  
-  //   const modalIsOpenState = false;
-  //   const basesState = TestBases;
-  //   const milksState = TestMilks;
-  //   const extrasState = TestExtras;
-  //   const menuItemsState = TestMenu;
-  //   const currentFrappeState = testFrappe;
-  //   const queueState = [testFrappe];
-
-  //   React.useState = jest.fn()
-  //    .mockReturnValueOnce([modalIsOpenState, {}])
-  //    .mockReturnValueOnce([basesState, {}])
-  //    .mockReturnValueOnce([milksState, {}])
-  //    .mockReturnValueOnce([extrasState, {}])
-  //    .mockReturnValueOnce([menuItemsState, {}])
-  //    .mockReturnValueOnce([currentFrappeState, {}])
-  //    .mockReturnValueOnce([queueState, {}]);
-
-  //   render(
-  //   <BrowserRouter>
-  //     <Queue></Queue>
-  //   </BrowserRouter>
-  //   );
-
-  //   expect(screen.getByText(/John Doe/i)).toBeInTheDocument();
-  // });
 });
