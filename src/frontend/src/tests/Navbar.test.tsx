109bdf6566aff01ab2eae54f97921eb1d92dd461
Dylando22 <dyl2elite@gmail.com>
2022-12-04 16:34:39 -0700

    try again

--- src/frontend/src/tests/Navbar.test.tsx
+++ src/frontend/src/tests/Navbar.test.tsx
@@ -3,121 +3,129 @@ import '@testing-library/jest-dom';
 import { fireEvent, render, screen } from '@testing-library/react';
 import NavBar from '../components/NavBar';
 import { BrowserRouter } from 'react-router-dom';
-// var mockPages = [
-//   {
-//       title: 'Home',
-//       path: '/home-page'
-//     },
-//     {
-//       title: 'Order Status',
-//       path: '/order-status'
-//     },
-//     {
-//       title: 'Menu',
-//       path: '/menu'
-//     },
-//     {
-//       title: 'Account',
-//       path: '/account'
-//     },
-//     {
-//       title: 'Login',
-//       path: '/'
-//     },
-//     {
-//       title: 'New User',
-//       path: '/new-user'
-//     },
-// ];
+var mockPages = [
+  {
+    title: 'Home',
+    path: '/home-page',
+  },
+  {
+    title: 'Order Status',
+    path: '/order-status',
+  },
+  {
+    title: 'Menu',
+    path: '/menu',
+  },
+  {
+    title: 'Account',
+    path: '/account',
+  },
+  {
+    title: 'Login',
+    path: '/',
+  },
+  {
+    title: 'New User',
+    path: '/new-user',
+  },
+];
 
-// function mockSetPages(arr:{ title: string; path: string; }[]) {
-//   mockPages = arr;
-// }
+function mockSetPages(arr: { title: string; path: string }[]) {
+  mockPages = arr;
+}
 
-// describe('NavBar Tests', () =>{
-//   test('renders NavBar Component', () => {
-//       render(
-//       <BrowserRouter>
-//       <NavBar pages={mockPages} />
-//       </BrowserRouter>);
-//     });
+describe('NavBar Tests', () => {
+  test('renders NavBar Component', () => {
+    render(
+      <BrowserRouter>
+        <NavBar pages={mockPages} setPages={() => {}} />
+      </BrowserRouter>
+    );
+  });
 
-//     test('Test NavBar Fields', () => {
-//       render(
-//       <BrowserRouter>
-//       <NavBar pages={mockPages} />
-//       </BrowserRouter>);
-//       expect(screen.getByText(/Home/i)).toBeInTheDocument();
-//       expect(screen.getByText(/Order Status/i)).toBeInTheDocument();
-//       expect(screen.getByText(/Menu/i)).toBeInTheDocument();
-//       expect(screen.getByText(/Account/i)).toBeInTheDocument();
-//       expect(screen.getByText(/Login/i)).toBeInTheDocument();
-//       expect(screen.getByText(/New User/i)).toBeInTheDocument();
-//     });
+  test('Test NavBar Fields', () => {
+    render(
+      <BrowserRouter>
+        <NavBar pages={mockPages} setPages={() => {}} />
+      </BrowserRouter>
+    );
+    expect(screen.getByText(/Home/i)).toBeInTheDocument();
+    expect(screen.getByText(/Order Status/i)).toBeInTheDocument();
+    expect(screen.getByText(/Menu/i)).toBeInTheDocument();
+    expect(screen.getByText(/Account/i)).toBeInTheDocument();
+    expect(screen.getByText(/Login/i)).toBeInTheDocument();
+    expect(screen.getByText(/New User/i)).toBeInTheDocument();
+  });
 
-//     test('Test Home Btn Click', async () => {
-//       render(
-//       <BrowserRouter>
-//       <NavBar pages={mockPages} />
-//       </BrowserRouter>);
+  test('Test Home Btn Click', async () => {
+    render(
+      <BrowserRouter>
+        <NavBar pages={mockPages} setPages={() => {}} />
+      </BrowserRouter>
+    );
 
-//       await fireEvent.click(screen.getByLabelText(/Home/i))
-//       const response = await screen.getByText(/Home/i)
-//       expect(response).toBeInTheDocument();
-//     });
+    await fireEvent.click(screen.getByLabelText(/Home/i));
+    const response = await screen.getByText(/Home/i);
+    expect(response).toBeInTheDocument();
+  });
 
-//     test('Test Menu Btn Click', async () => {
-//       render(
-//       <BrowserRouter>
-//       <NavBar pages={mockPages} />
-//       </BrowserRouter>);
+  test('Test Menu Btn Click', async () => {
+    render(
+      <BrowserRouter>
+        <NavBar pages={mockPages} setPages={() => {}} />
+      </BrowserRouter>
+    );
 
-//       await fireEvent.click(screen.getByLabelText(/Menu/i))
-//       const response = await screen.getByText(/Menu/i)
-//       expect(response).toBeInTheDocument();
-//     });
+    await fireEvent.click(screen.getByLabelText(/Menu/i));
+    const response = await screen.getByText(/Menu/i);
+    expect(response).toBeInTheDocument();
+  });
 
-//     test('Test Order Status Btn Click', async () => {
-//       render(
-//       <BrowserRouter>
-//       <NavBar pages={mockPages} />
-//       </BrowserRouter>);
+  test('Test Order Status Btn Click', async () => {
+    render(
+      <BrowserRouter>
+        <NavBar pages={mockPages} setPages={() => {}} />
+      </BrowserRouter>
+    );
 
-//       await fireEvent.click(screen.getByLabelText(/Order Status/i))
-//       const response = await screen.getByText(/Order Status/i)
-//       expect(response).toBeInTheDocument();
-//     });
+    await fireEvent.click(screen.getByLabelText(/Order Status/i));
+    const response = await screen.getByText(/Order Status/i);
+    expect(response).toBeInTheDocument();
+  });
 
-//     test('Test Account Btn Click', async () => {
-//       render(
-//       <BrowserRouter>
-//       <NavBar pages={mockPages} />
-//       </BrowserRouter>);
+  test('Test Account Btn Click', async () => {
+    render(
+      <BrowserRouter>
+        <NavBar pages={mockPages} setPages={() => {}} />
+      </BrowserRouter>
+    );
 
-//       await fireEvent.click(screen.getByLabelText(/Account/i))
-//       const response = await screen.getByText(/Account/i)
-//       expect(response).toBeInTheDocument();
-//     });
+    await fireEvent.click(screen.getByLabelText(/Account/i));
+    const response = await screen.getByText(/Account/i);
+    expect(response).toBeInTheDocument();
+  });
 
-//     test('Test Login Btn Click', async () => {
-//       render(
-//       <BrowserRouter>
-//       <NavBar pages={mockPages} />
-//       </BrowserRouter>);
+  test('Test Login Btn Click', async () => {
+    render(
+      <BrowserRouter>
+        <NavBar pages={mockPages} setPages={() => {}} />
+      </BrowserRouter>
+    );
 
-//       await fireEvent.click(screen.getByLabelText(/Login/i))
-//       const response = await screen.getByText(/Login/i)
-//       expect(response).toBeInTheDocument();
-//     });
+    await fireEvent.click(screen.getByLabelText(/Login/i));
+    const response = await screen.getByText(/Login/i);
+    expect(response).toBeInTheDocument();
+  });
 
-//     test('Test New User Btn Click', async () => {
-//       render(
-//       <BrowserRouter>
-//       <NavBar pages={mockPages} />
-//       </BrowserRouter>);
+  test('Test New User Btn Click', async () => {
+    render(
+      <BrowserRouter>
+        <NavBar pages={mockPages} setPages={() => {}} />
+      </BrowserRouter>
+    );
 
-//       await fireEvent.click(screen.getByLabelText(/New User/i))
-//       const response = await screen.getByText(/New User/i)
-//       expect(response).toBeInTheDocument();
-//     });
-// })
+    await fireEvent.click(screen.getByLabelText(/New User/i));
+    const response = await screen.getByText(/New User/i);
+    expect(response).toBeInTheDocument();
+  });
+});
